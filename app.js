4f7da3ede0a8f82001eafa6cccfa7ba51d36e36e
olivahr <olivahr01@gmail.com>
2026-01-17 17:29:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-// LOGIN FUNCIONAL
-function login(){
+function login() {
   const email = document.getElementById("email").value.trim();
   const pass = document.getElementById("password").value.trim();
 
-  if(email === "empleados@company.com" && pass === "SunPower2026"){
-    sessionStorage.setItem("auth","true");
+  if(email === "empleados@company.com" && pass === "SunPower2026") {
     window.location.href = "details.html";
   } else {
     alert("Invalid corporate credentials");
   }
-}
-
-// SLIDER FUNCIONAL
-function slideNext(btn){
-  const slider = btn.parentElement.querySelector('.slider');
-  slider.scrollBy({left: slider.offsetWidth, behavior:'smooth'});
-}
-function slidePrev(btn){
-  const slider = btn.parentElement.querySelector('.slider');
-  slider.scrollBy({left: -slider.offsetWidth, behavior:'smooth'});
 }
